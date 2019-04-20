a884132d4e83db500012c231ec9eaa202e33cb90
triano123 <triano1933@gmail.com>
2019-04-20 20:36:42 +0700

    add module that we need

--- lstm_code.py
+++ lstm_code.py
@@ -1,2 +1,28 @@
 import pandas as pd 
 import numpy as np 
+
+#visualization
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+#module deep learning 
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dropout
+from keras.layers import Dense
+from keras.layers import LSTM
+
+#ploting with keras
+from livelossplot import PlotLossesKeras
+
+#tensorboard 
+from keras.callbacks import TensorBoard
+from time import time
+
+#math module
+import math
+from math import sqrt
+
+#save model 
+from keras.models import load_model
